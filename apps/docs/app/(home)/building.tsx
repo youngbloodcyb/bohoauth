45b5e069dab539a05cb1912bbcb4ef175847486f
Cameron Youngblood <cameron.youngblood@gmail.com>
2025-01-20 10:33:13 -0800

    update to 2025

--- apps/docs/app/(home)/building.tsx
+++ apps/docs/app/(home)/building.tsx
@@ -63,7 +63,7 @@ export function Building() {
               <span>2</span>
               <span>0</span>
               <span>2</span>
-              <span>4</span>
+              <span>5</span>
             </div>
           </div>
         </div>
